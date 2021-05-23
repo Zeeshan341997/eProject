41ca57e846056fabfcef9c85f811cc95cd6c9e08
Zeeshan341997 <zeeshanzeegota@gmail.com>
2021-05-24 00:55:53 +0500

    update

--- js/script.js
+++ js/script.js
@@ -34,13 +34,10 @@ function mySliderUp(){
     document.getElementById("slide").style.top = "-100%";
     document.getElementById("slide").style.animation = "fadeInUp";
     document.getElementById("slide").style.animationDuration = "0.8s";
-    
 }
 function mySliderDown(){
     document.getElementById("slide").style.top = "0";
     document.getElementById("slide").style.animation = "fadeInDown";
     document.getElementById("slide").style.animationDuration = "0.8s";
 }
-
-
 
